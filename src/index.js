9f45d4d99deba871fa1234f35118497cd4dc6fb0
pengx12 <55561352+pengx12@users.noreply.github.com>
2020-03-08 11:36:33 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import { Scene, PointLayer, Popup } from "@antv/l7";
+import { GaodeMap } from "@antv/l7-maps";
+
+const scene = new Scene({
+  id: "map",
+  map: new GaodeMap({
+    pitch: 0,
+    style: "dark",
+    center: [112, 23.69],
+    zoom: 1.5
+  })
+});
+
+fetch(
+  "https://raw.githubusercontent.com/pengx12/sandboxtry/master/src/whc-sites-2019.csv"
+)
+  .then(res => res.text())
+  .then(data => {
+    const pointLayer = new PointLayer({})
+      .source(data, {
+        parser: {
+          type: "csv",
+          x: "longitude",
+          y: "latitude"
+        }
+      })
+      .shape("circle")
+      .active(true)
+      .animate(true)
+      .size(36)
+      .color("#4cfd47")
+      .style({
+        opacity: 1
+      });
+    pointLayer.on("mousemove", e => {
+      const popup = new Popup({
+        offsets: [0, 0],
+        closeButton: false
+      })
+        .setLnglat(e.lngLat)
+        .setHTML(
+          `<span>${e.feature.name_en}\n\n ${e.feature.states_name_en}</span>`
+        );
+      scene.addPopup(popup);
+    });
+    scene.addLayer(pointLayer);
+  });
